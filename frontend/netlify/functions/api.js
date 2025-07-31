b4706aa61c816f4b7dc3e61081baaa2a31303afe
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-07-31 20:34:40 +0530

    refactor: remove unused path resolution utility and simplify handler import

--- frontend/netlify/functions/api.js
+++ frontend/netlify/functions/api.js
@@ -1,10 +1,6 @@
 // netlify/functions/api.js
 
-import path from 'path';
-import { __dirname } from '../../utils/esm-paths.js'; // no re-declaration
-
 export const handler = async (event, context) => {
-  const handlerPath = path.resolve(__dirname, '../../../backend/handler.js');
-  const { handler } = await import(handlerPath);
+  const { handler } = await import('../../../../backend/handler.js');
   return handler(event, context);
 };
--- frontend/utils/esm-paths.js
+++
@@ -1,8 +0,0 @@
-// utils/esm-paths.js
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export { __filename, __dirname };
